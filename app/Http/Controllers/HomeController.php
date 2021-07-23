f998cbd9c8e2564177cce003e4d45882b3c1b106
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-07-23 09:47:09 +0900

    view-composer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,12 +28,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //メモデータを取得
-        $memos = Memo::select('memos.*')->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')->orderBy('updated_at', 'DESC')->get();
         $tags = Tag::where('user_id', '=', \Auth::id())->whereNull('deleted_at')
                     ->orderBy('id', 'DESC')->get();
-        return view('create', compact('memos', 'tags'));
+        return view('create', compact('tags'));
     }
 
     public function store (Request $request) {
@@ -61,8 +58,6 @@ class HomeController extends Controller
     public function edit($id)
     {
         //メモデータを取得
-        $memos = Memo::select('memos.*')->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')->orderBy('updated_at', 'DESC')->get();
         $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
                         ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
                         ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
@@ -76,7 +71,7 @@ class HomeController extends Controller
         }
         $tags = Tag::where('user_id', '=', \Auth::id())->whereNull('deleted_at')
                     ->orderBy('id', 'DESC')->get();
-        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
+        return view('edit', compact('edit_memo', 'include_tags', 'tags'));
     }
 
     public function update (Request $request) {
