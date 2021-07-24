84dc7dabe83c1f5b97351a3b51da6da3b79c241f
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-07-24 13:47:24 +0900

    search-tag-refactoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,23 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('*', function($view) {
-            $query_tag = \Request::query('tag');
-            //もしクエリパラメータがあればタグで絞り込み
-            if(!empty($query_tag)) {
-                $memos = Memo::select('memos.*')
-                    ->leftjoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
-                    ->where('memo_tags.tag_id', '=', $query_tag)
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            } else {
-                $memos = Memo::select('memos.*')
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            }
+            //インスタンス化
+            $memo_model = new Memo();
+            //メモ取得
+            $memos = $memo_model->getMyMemo();
             $tags = Tag::where('user_id', '=', \Auth::id())
                     ->whereNull('deleted_at')->orderBy('id', 'DESC')->get();
             $view->with('memos', $memos)->with('tags', $tags);
