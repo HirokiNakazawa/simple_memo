3c93776c27ad1b3d0ccf652ddae5a818034e8cf7
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-07-24 21:45:06 +0900

    css

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,15 +6,15 @@
 
 @section('content')
 <div class="card">
-    <div class="card-header">
+    <div class="card-header d-flex justify-content-between">
         メモ編集
-        <form class="card-body" id="delete-form" action="{{ route('destroy') }}" method="post">
+        <form id="delete-form" action="{{ route('destroy') }}" method="post">
             @csrf
             <input type="hidden" name="memo_id" value="{{$edit_memo[0]['id']}}">
-            <i class="fas fa-trash" onclick="deleteHandle(event);"></i>
+            <i class="fas fa-trash mr-3" onclick="deleteHandle(event);"></i>
         </form>
     </div>
-        <form class="card-body" action="{{ route('update') }}" method="post">
+        <form class="card-body my-card-body" action="{{ route('update') }}" method="post">
             @csrf
             <input type="hidden" name="memo_id" value="{{$edit_memo[0]['id']}}">
             <div class="mb-3">
